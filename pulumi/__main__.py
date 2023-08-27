c08e01a94c1df71d7a2c56cfcbf944c1124c1723
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2023-08-28 03:08:09 +0900

    feat: :+1: import WebApp from Firebase

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -3,17 +3,24 @@ import pulumi_gcp as gcp
 
 import pulumi
 
-defaultProject = gcp.firebase.Project(
+default_firebase_project = gcp.firebase.Project(
     "default",
     project="yomutube-f5b5d",
     opts=pulumi.ResourceOptions(protect=True),
 )
 
-pulumiServiceAccount = gcp.serviceaccount.Account(
+pulumi_service_account = gcp.serviceaccount.Account(
     "pulumi",
     account_id="pulumi",
     description="Service Account for Pulumi",
     display_name="pulumi",
+    project="yomutube-f5b5d",
+    opts=pulumi.ResourceOptions(protect=True),
+)
+
+default_web_app = gcp.firebase.WebApp(
+    "default",
+    display_name="yomutube (web)",
     project="yomutube-f5b5d",
     opts=pulumi.ResourceOptions(protect=True),
 )
