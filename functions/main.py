02a02a50e69640c7a15e3bc2b8f5e23866589743
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2023-11-28 01:42:02 +0900

    feat: :+1: init functions

---
+++ functions/main.py
@@ -0,0 +1,9 @@
+from firebase_admin import initialize_app
+from firebase_functions import https_fn
+
+initialize_app()
+
+
+@https_fn.on_request()
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world!")
